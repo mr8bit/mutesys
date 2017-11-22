914c793360189f839c4b0f33e4f7ed422cc1de42
mr8bit <69kissmysausage@gmail.com>
2017-11-22 21:58:02 +0300

    up

--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,5 @@
 from channels import route
-from .consumers import ws_connect, ws_receive, ws_disconnect, chat_join, chat_leave, chat_send, qa_send
+from .consumers import ws_connect, ws_receive, ws_disconnect, chat_join, chat_leave, chat_send, qa_send, qa_answer
 
 
 # There's no path matching on these routes; we just rely on the matching
