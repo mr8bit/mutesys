64ef23a4efb6c0830752357c17a21129e09bfdf1
mr8bit <69kissmysausage@gmail.com>
2017-12-01 02:38:52 +0300

    up server

--- multichat/urls.py
+++ multichat/urls.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
-from chat.views import index
 
+from chat.views import index
 
 urlpatterns = [
     url(r'^$', index),
     url(r'^admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
